b8b0ec9dcbb5c2fcd81d5e9ff1f9720388df9389
Iman Ghafoori <imanghafoori1@gmail.com>
2021-09-12 02:22:32 +0430

    init

---
+++ src/QuestionServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Imanghafoori;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuestionServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
